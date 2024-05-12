3f10f0a74cceb88088aa15fdb907d38ab31fe31b
Lucas Martins <lucas.martins.fsa@gmail.com>
2024-05-12 19:24:23 -0300

    implementa Dead Letter Queue

--- src/main/java/com/devlucasmart/propostaapi/comum/config/RabbitMQConfiguration.java
+++ src/main/java/com/devlucasmart/propostaapi/comum/config/RabbitMQConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -28,25 +29,74 @@ public class RabbitMQConfiguration {
     @Bean
     public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
         return QueueBuilder.durable("proposta-pendente.ms-analise-credito")
+                .deadLetterExchange("proposta-pendente-dlx.ex")
                 .build();
     }
 
     @Bean
     public Queue criarFilaPropostaPendenteNoticacaoMsNotificacao() {
         return QueueBuilder.durable("proposta-pendente.ms-notificacao")
+                .deadLetterExchange("proposta-pendente-dlx.ex")
                 .build();
     }
 
     @Bean
     public Queue criarFilaPropostaConcluidaMsProposta() {
         return QueueBuilder.durable("proposta-concluida.ms-proposta")
+                .deadLetterExchange("proposta-concluida-dlx.ex")
                 .build();
     }
 
     @Bean
     public Queue criarFilaPropostaConcluidaNoticacaoMsNotificacao() {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao")
+                .deadLetterExchange("proposta-pendente-ms-notificacao-dlx.ex")
                 .build();
+    }
+    @Bean
+    public Queue criarFilaPropostaPendenteDlq() {
+        return QueueBuilder.durable("proposta-pendente.dlq").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaPendenteNoticacaoMsNotificacaoDlq() {
+        return QueueBuilder.durable("proposta-pendente-Ms-Notificacao.dlq").build();
+    }
+
+    @Bean
+    public Queue criarFilaPropostaConcluidaDlq() {
+        return QueueBuilder.durable("proposta-concluida.dlq").build();
+    }
+
+    @Bean
+    public FanoutExchange deadLetterExchangePropostaPendente() {
+        return ExchangeBuilder.fanoutExchange("proposta-pendente-dlx.ex").build();
+    }
+
+    @Bean
+    public FanoutExchange deadLetterExchangePropostaPendenteMsNotificacao() {
+        return ExchangeBuilder.fanoutExchange("proposta-pendente-ms-notificacao-dlx.ex").build();
+    }
+
+    @Bean
+    public FanoutExchange deadLetterExchangePropostaConcluida() {
+        return ExchangeBuilder.fanoutExchange("proposta-concluida-dlx.ex").build();
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendente(){
+        return BindingBuilder.bind(criarFilaPropostaPendenteDlq()).to(deadLetterExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaConcluida(){
+        return BindingBuilder.bind(criarFilaPropostaConcluidaDlq()).to(deadLetterExchangePropostaConcluida());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsNotificacao(){
+        return BindingBuilder.bind(criarFilaPropostaPendenteNoticacaoMsNotificacaoDlq())
+                .to(deadLetterExchangePropostaPendenteMsNotificacao());
     }
 
     @Bean
@@ -94,7 +144,7 @@ public class RabbitMQConfiguration {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+    public MessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
